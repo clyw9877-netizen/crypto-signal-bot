537d806add806b7f9c7efac55859f43742b69978
clyw9877-netizen <clyw9877@gmail.com>
2026-07-12 14:16:48 -0700

    Refactor config to use environment variables

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 # Crypto Signal Bot Config
-TELEGRAM_TOKEN = "8824947957:AAEmr0pByUHZ_LrM3QD4AQ4HPGub_WNzpoQ"
-TELEGRAM_CHANNEL = "-1004369003536"
+import os
 
-BINGX_API_KEY = ""
-BINGX_SECRET = ""
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "")
+TELEGRAM_CHANNEL = os.environ.get("TELEGRAM_CHANNEL", "-1004369003536")
 
-CMC_API_KEY = "856adf34d92742cda13c899b36a23a59"
+BINGX_API_KEY = os.environ.get("BINGX_API_KEY", "")
+BINGX_SECRET = os.environ.get("BINGX_SECRET", "")
+
+CMC_API_KEY = os.environ.get("CMC_API_KEY", "")
 
 VIRTUAL_DEPOSIT = 100.0
 RISK_PER_TRADE = 1.0
@@ -14,19 +16,19 @@ PRICE_CHECK_INTERVAL = 10
 MORNING_DIGEST_TIME = "08:00"
 
 COINS = [
-    "BTC-USDT", "ETH-USDT", "SOL-USDT", "BNB-USDT", "XRP-USDT",
-    "DOGE-USDT", "ADA-USDT", "AVAX-USDT", "DOT-USDT", "MATIC-USDT",
-    "LTC-USDT", "LINK-USDT", "UNI-USDT", "ATOM-USDT", "ETC-USDT",
-    "TRX-USDT", "NEAR-USDT", "FTM-USDT", "OP-USDT", "ARB-USDT",
-    "SUI-USDT", "INJ-USDT", "WIF-USDT", "PEPE-USDT", "SHIB-USDT",
+        "BTC-USDT", "ETH-USDT", "SOL-USDT", "BNB-USDT", "XRP-USDT",
+        "DOGE-USDT", "ADA-USDT", "AVAX-USDT", "DOT-USDT", "MATIC-USDT",
+        "LTC-USDT", "LINK-USDT", "UNI-USDT", "ATOM-USDT", "ETC-USDT",
+        "TRX-USDT", "NEAR-USDT", "FTM-USDT", "OP-USDT", "ARB-USDT",
+        "SUI-USDT", "INJ-USDT", "WIF-USDT", "PEPE-USDT", "SHIB-USDT",
 ]
 
 CONFIDENCE_MATRIX = [
-    (50, 60, 20, 5),
-    (60, 70, 40, 10),
-    (70, 80, 60, 15),
-    (80, 90, 80, 20),
-    (90, 101, 100, 30),
+        (50, 60, 20, 5),
+        (60, 70, 40, 10),
+        (70, 80, 60, 15),
+        (80, 90, 80, 20),
+        (90, 101, 100, 30),
 ]
 
 BINGX_FEE = 0.00075
